282bb06e92095321ddd3c1e179c9e1ac6af5e791
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-05-04 07:38:08 -0400

    Added 'productID' function to prompt user with two questions

--- Bamazon/bamazonCustomer.js
+++ Bamazon/bamazonCustomer.js
@@ -15,37 +15,37 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    queryAllProducts();
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  queryAllProducts();
 });
 
 
-function start() {
-    inquirer
-        .prompt({
-            name: "itemID",
-            type: "list",
-            message: "Please enter an ID from 1-10 of the product you would like to purchase",
-            choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        })
-        .then(function(choice) {
-            if (choice.itemID === res[i].ID) {
-                showProduct();
-            } else{
-              connection.end();
-            }
-        })
+function productID() {
+  inquirer
+  .prompt([
+    {
+      name: "itemID",
+      type: "input",
+      message: "Please enter an ID from 1-10 of the product you would like to purchase!"
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "How many would you like to purchase?"
+    }
+  ])
 }
+
+
 
 function queryAllProducts() {
-    connection.query("SELECT * FROM products", function(err,res) {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].ID + " | " + res[i].Product + " | " + res[i].Department + " | " + res[i].Price + " | " + res[i].Quantity);
-        }
-        console.log("----------------------------------");
-        console.log(res);
-    })
+  connection.query("SELECT * FROM products", function(err,res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i].ID + " | " + res[i].Product + " | " + res[i].Department + " | " + res[i].Price + " | " + res[i].Quantity);
+    }
+    console.log("----------------------------------");
+    console.log(res);
+  })
 }
-
